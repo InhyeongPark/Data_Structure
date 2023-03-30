ba8f12c7bfb031339c85b73026ecb08f7b8f9900
Inhyeong Park <56657115+InhyeongPark@users.noreply.github.com>
2023-03-30 14:28:36 -0400

    Add Deque

---
+++ Deque/LinkedDeque.java
@@ -0,0 +1,185 @@
+public class LinkedDeque<T> {
+
+    // Do not add new instance variables or modify existing ones.
+    private LinkedNode<T> head;
+    private LinkedNode<T> tail;
+    private int size;
+
+    // Do not add a constructor.
+
+    /**
+     * A private method that checks if the data is null.
+     *
+     * @param data the data that we will check if it's null or not
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    private void nullCheck(T data) {
+        if (data == null) {
+            throw new java.lang.IllegalArgumentException("Null data can not be inserted in the array");
+        }
+    }
+
+    /**
+     * Adds the element to the front of the deque.
+     *
+     * Must be O(1).
+     *
+     * @param data the data to add to the front of the deque
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void addFirst(T data) {
+        nullCheck(data);
+        LinkedNode<T> newNode = new LinkedNode<>(data);
+        if (size == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            newNode.setNext(head);
+            head.setPrevious(newNode);
+            head = newNode;
+        }
+        size++;
+    }
+
+    /**
+     * Adds the element to the back of the deque.
+     *
+     * Must be O(1).
+     *
+     * @param data the data to add to the back of the deque
+     * @throws java.lang.IllegalArgumentException if data is null
+     */
+    public void addLast(T data) {
+        nullCheck(data);
+        LinkedNode<T> newNode = new LinkedNode<>(data);
+        if (size == 0) {
+            head = newNode;
+            tail = newNode;
+        } else {
+            tail.setNext(newNode);
+            newNode.setPrevious(tail);
+            tail = newNode;
+        }
+        size++;
+    }
+
+    /**
+     * A private method that checks if the deque is empty.
+     *
+     * @throws java.util.NoSuchElementException if the deque is empty
+     */
+    private void elementCheck() {
+        if (size == 0) {
+            throw new java.util.NoSuchElementException("We can't find the element because deque is empty.");
+        }
+    }
+
+    /**
+     * Removes and returns the first element of the deque.
+     *
+     * Must be O(1).
+     *
+     * @return the data formerly located at the front of the deque
+     * @throws java.util.NoSuchElementException if the deque is empty
+     */
+    public T removeFirst() {
+        elementCheck();
+        T currentData = head.getData();
+        if (size == 1) {
+            head = null;
+            tail = null;
+        } else {
+            head = head.getNext();
+            head.setPrevious(null);
+        }
+        size--;
+        return currentData;
+    }
+
+    /**
+     * Removes and returns the last element of the deque.
+     *
+     * Must be O(1).
+     *
+     * @return the data formerly located at the back of the deque
+     * @throws java.util.NoSuchElementException if the deque is empty
+     */
+    public T removeLast() {
+        elementCheck();
+        T currentData = tail.getData();
+        if (size == 1) {
+            head = null;
+            tail = null;
+        } else {
+            tail = tail.getPrevious();
+            tail.setNext(null);
+        }
+        size--;
+        return currentData;
+    }
+
+    /**
+     * Returns the first data of the deque without removing it.
+     *
+     * Must be O(1).
+     *
+     * @return the data located at the front of the deque
+     * @throws java.util.NoSuchElementException if the deque is empty
+     */
+    public T getFirst() {
+        elementCheck();
+        return head.getData();
+    }
+
+    /**
+     * Returns the last data of the deque without removing it.
+     *
+     * Must be O(1).
+     *
+     * @return the data located at the back of the deque
+     * @throws java.util.NoSuchElementException if the deque is empty
+     */
+    public T getLast() {
+        elementCheck();
+        return tail.getData();
+    }
+
+    /**
+     * Returns the head node of the deque.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return node at the head of the deque
+     */
+    public LinkedNode<T> getHead() {
+        // DO NOT MODIFY THIS METHOD!
+        return head;
+    }
+
+    /**
+     * Returns the tail node of the deque.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return node at the head of the deque
+     */
+    public LinkedNode<T> getTail() {
+        // DO NOT MODIFY THIS METHOD!
+        return tail;
+    }
+
+    /**
+     * Returns the size of the deque.
+     *
+     * For grading purposes only. You shouldn't need to use this method since
+     * you have direct access to the variable.
+     *
+     * @return the size of the deque
+     */
+    public int size() {
+        // DO NOT MODIFY THIS METHOD!
+        return size;
+    }
+}
